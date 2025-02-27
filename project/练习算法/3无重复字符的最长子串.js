c39fa4d4ef081a02e7f593a15c585d7d4078d384
shenqilong <2169963939@qq.com>
2025-02-27 16:50:00 +0800

    算法练习

--- project/练习算法/3无重复字符的最长子串.js
+++ project/练习算法/3无重复字符的最长子串.js
@@ -4,12 +4,23 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let _arr = s.split('')
-    let max = 0
-    for (let i = 0; i < _arr.length; i++) {
-        let ac = _arr[i], move = _arr[i]
-        
+  let left = 0; // 滑动窗口的左边界
+  let maxLength = 0; // 存储最长子串长度的变量
+  let charSet = new Set(); // 用于存储当前窗口中的字符
+
+  for (let right = 0; right < s.length; right++) {
+    // 当发现重复字符时，缩小窗口直到重复字符被移除
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
     }
-    return max
+
+    // 将当前字符添加到集合中
+    charSet.add(s[right]);
+    // 更新最长子串长度
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
 };
 console.log(lengthOfLongestSubstring("pwwkew"));
--- project/练习算法/index.js
+++ project/练习算法/index.js
@@ -1,5 +1,5 @@
 // 给定一个含有 n 个正整数的数组和一个正整数 target 。
-// 找出该数组中满足其总和大于等于 target 的长度最小的 
+// 找出该数组中满足其总和大于等于 target 的长度最小的
 // 子数组
 //  [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
 /**
@@ -7,6 +7,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSubArrayLen = function(target, nums) {
-    
+var minSubArrayLen = function (target, nums) {
+  let left = 0; // 滑动窗口的左边界
+  let sum = 0; // 当前窗口的总和
+  let minLen = Infinity; // 存储最小长度的变量，初始设为无穷大
+
+  // 遍历数组，right 是滑动窗口的右边界
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right]; // 将当前元素加到窗口的总和中
+
+    // 当总和大于等于 target 时，尝试缩小窗口
+    while (sum >= target) {
+      // 更新最小长度
+      minLen = Math.min(minLen, right - left + 1);
+      // 减去窗口左边的元素，并移动左边界
+      sum -= nums[left];
+      left++;
+    }
+  }
+
+  // 如果 minLen 没有被更新，说明没有找到符合条件的子数组
+  return minLen === Infinity ? 0 : minLen;
 };
+
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
